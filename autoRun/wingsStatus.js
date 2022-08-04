27d3fb08cd14222cf25107af10600ff83b4abcc8
Henry Cooper <lilghostyninja@gmail.com>
2022-08-04 11:13:18 +0000

    Functionability Update

--- autoRun/wingsStatus.js
+++ autoRun/wingsStatus.js
@@ -10,29 +10,29 @@ module.exports = async (client) => {
 
 
         
-        var node1ping = ['n1.luxxy.host'];
+        var node1ping = [config.PingIP.IP1];
         node1ping.forEach(function(node1png){
         ping.sys.probe(node1png, function(isAlive){
             var node1 = isAlive ? '<:serveronline:1003694099100078211> Operational':'<:serveroffline:1003693981223362642> Services Down';
  
 
-        var node2ping = ['n2.luxxy.host'];
+        var node2ping = [config.PingIP.IP2];
         node2ping.forEach(function(node2png){
         ping.sys.probe(node2png, function(isAlive){
             var node2 = isAlive ? '<:serveronline:1003694099100078211> Operational':'<:serveroffline:1003693981223362642> Services Down';
 
 
-        var panelping = ['panel.luxxy.host'];
+        var panelping = [config.PingIP.IP3];
         panelping.forEach(function(pnlpng){
         ping.sys.probe(pnlpng, function(isAlive){
             var panel = isAlive ? '<:serveronline:1003694099100078211> Operational':'<:serveroffline:1003693981223362642> Services Down';
 
-        var webping = ['cloud-net.cf'];
+        var webping = [config.PingIP.IP4];
         webping.forEach(function(webpng){
             ping.sys.probe(webpng, function(isAlive){
                 var web = isAlive ? '<:serveronline:1003694099100078211> Operational':'<:serveroffline:1003693981223362642> Services Down';
 
-        var testpanelping = ['testtttttt.luxxy.host'];
+        var testpanelping = [config.PingIP.IP5];
         testpanelping.forEach(function(testpnlpng){
             ping.sys.probe(testpnlpng, function(isAlive){
                 var testpanel = isAlive ? '<:serveronline:1003694099100078211> Operational':'<:serveroffline:1003693981223362642> Services Down';
@@ -42,9 +42,11 @@ module.exports = async (client) => {
               console.log(panel);
               console.log(web);
 
+// CONFIG STARTS HERE - Change The Emoji;s Above and the Names Below. The Names are on the .setDescription Place.
+
         let embed = [
             new Discord.MessageEmbed()
-            .setTitle(`Cloud Network Node Status:`)
+            .setTitle(`IP Status:`)
             .setDescription(`**Status**\n\nNode 1: ${node1}\nNode 2: ${node2}\n\n**Misc Status**\n\nPanel: ${panel}\nWebsite:${web}\nBackup Server: ${testpanel}\n*Updating every \`${config.settings.nodeStatusDelay} seconds\`*`)
             .setColor(`#677bf9`)
             .setTimestamp()
